efe72cc7dd00b893370dd8b2f0caae8fd3f4675e
auge-media <95193809+auge-media@users.noreply.github.com>
2024-10-09 01:28:12 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
-<?php
-echo "HOLA MUNDO"
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Example PHP page</title>
+  </head>
+  <body>
+    <?php
+      // Parámetros de conexión a la base de datos
+      $servername = "mysql-practicas.alwaysdata.net";  // Dirección del servidor (generalmente "localhost" en un servidor local)
+      $username = "practicas";   // Usuario de la base de datos
+      $password = "coco12y7"; // Contraseña de la base de datos
+      $dbname = "practicas_pruebas"; // Nombre de la base de datos
+
+      // Crear conexión
+      $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+      // Verificar la conexión
+      if (!$conn) {
+        die("Conexión fallida: " . mysqli_connect_error());
+      }
+      echo '<h1>Conexión exitosa a la base de datos</h1>';
+
+      // Cerrar conexión
+      mysqli_close($conn);
+
+      // Información PHP
+      echo '<p>This page uses PHP version ' . phpversion() . '.</p>';
+    ?>
+  </body>
+</html>
